406f51562ac538ce1f92ee3a83b49507aa1f9874
Daniel Kipp <daniel.kipp@gmail.com>
2023-02-01 17:32:46 +0100

    added user quote and average rating to index

--- app/models/list.rb
+++ app/models/list.rb
@@ -6,4 +6,9 @@ class List < ApplicationRecord
   has_many :reviews, dependent: :destroy
 
   validates :name, uniqueness: true, presence: true
+
+  def ave_rating
+    ave = reviews.map(&:rating).sum.fdiv(reviews.length)
+    (ave * 2).round / 2.0 unless ave.nan?
+  end
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -2,5 +2,5 @@ class Review < ApplicationRecord
   belongs_to :list
 
   validates :comment, :rating, presence: true
-  validates :rating, inclusion: { in: [0, 1, 2, 3, 4, 5] }
+  validates :rating, inclusion: { in: [1, 2, 3, 4, 5] }
 end
